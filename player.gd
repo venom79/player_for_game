4081b2f0f3a0ca521e002bffc743badb712fbb4d
venom79 <93215520+venom79@users.noreply.github.com>
2021-12-25 11:46:53 +0530

    Add files via upload
    added animations

--- player.gd
+++ player.gd
@@ -6,6 +6,10 @@ const friction = 500
 
 var velocity = Vector2.ZERO
 
+onready var animation = $AnimationPlayer
+onready var animationtree = $AnimationTree
+onready var animationstate = animationtree.get("parameters/playback")
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,9 +17,13 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationtree.set("parameters/idle/blend_position", input_vector)
+		animationtree.set("parameters/run/blend_position", input_vector)
+		animationstate.travel("run")
 		velocity = velocity.move_toward(input_vector * max_speed, accelaration * delta)
 	
 	else:
+		animationstate.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO , friction * delta)
 		
 	velocity = move_and_slide(velocity)
